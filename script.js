88b279125f4ff2690888b949539f6ca48065bce1
angularjs80 <bigcen@naver.com>
2017-11-06 17:41:42 +0900

    reBase

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 // Api 호출항수 From By button
 function getSuccessOutput() {
     $.ajax({
-        url:'http://localhost:80/demo/test/getUserList',
+        url:'http://localhost:8080/demo/test/getUserList',
         complete: function (response) {
             var rtnJson = JSON.parse(response.responseText);
             setJson(rtnJson.rtnList);
+            setDataTables(rtnJson.rtnList);
+
         },
         error: function () {
             $('#output').html('Bummer: there was an error!');
@@ -14,7 +16,40 @@ function getSuccessOutput() {
     return false;
 }
 
-// Api 호출항수 From By getSuccessOutput
+// Json 데이터 화면에 표현 Call by getSuccessOutput(Use BootStrap Tables Lib)
+function setDataTables(jsonListData){
+    $('#example').DataTable(
+        {
+            data: jsonListData,
+            table: "#example",
+            columns: [
+                { data: "USER_ID" },
+                { data: "USER_PASSWORD" },
+                { data: "USER_NAME" },
+                { data: "USER_AGE" }
+            ]
+        }
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Json 데이터 화면에 Append Call By getSuccessOutput
 function setJson(jsonListData) {
     var showData = $('#show-data');
     var items = jsonListData.map(function (item) {
@@ -23,23 +58,19 @@ function setJson(jsonListData) {
 
     showData.empty();
     console.log(items.length);
+    console.log(jsonListData);
     if (items.length) {
-
         var startContent = '<div class="input-group" >'
                                 +'<span class="input-group-addon">'
                                     +'<input type="checkbox" >'
                                 +'</span>'
                                 +'<input type="text" class="form-control" value="';
-
         var endContent =    '"/><span class="input-group-btn">'
                                 +'<button class="btn btn-danger" type="button" >delete</button>'
                               +'</span>'
                          +'</div>';
-
         var content = startContent + items.join(endContent + startContent )+endContent;
-
         showData.append(content);
-
     }
 
 }
